55c1a0fba89e3c644eb7926de8a5f72940181afa
Buhle Mwanza <buhle.mwanza@gmail.com>
2018-01-02 10:24:18 -0600

    Start of the project. Including a .txt highlighting the purposes, functionality and main Model's of data for the System

---
+++ app/src/main/java/com/learn/buhle/gradulator/CourseListActivity.java
@@ -0,0 +1,13 @@
+package com.learn.buhle.gradulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+    }
+}
